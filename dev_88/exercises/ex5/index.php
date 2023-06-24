17040f4d8c1979c5503e8ea60e53d879a16b1a68
Nave Maymon <nave.may@gmail.com>
2023-06-24 13:10:08 +0300

    ex5

--- dev_88/exercises/ex5/index.php
+++ dev_88/exercises/ex5/index.php
@@ -80,7 +80,7 @@
                   echo        '<img src="' . $img . '"class="card-img-top">';
                   echo        '<div class="card-body">';
                   echo          '<h5 class="card-title text-center">' . $row["name_books"] . '</h5>';
-                  echo        '<div class="card-footer text-center"><a href="getbookweek.php?bookId=' . $row["id"] . '"class="btn btn-primary">More details!</a>';
+                  echo        '<div class="card-footer text-center"><a href="getbookweek.php?bookId="' . $row["id"] . '"class="btn btn-primary">More details!</a>';
                   echo '</div></div></div>';
                   }
                 ?>
